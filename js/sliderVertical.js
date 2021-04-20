a6ac37e507bac5824ccd7b2883f766131292be5f
DVolodymyr <xxvolodymyrxx@gmail.com>
2021-04-20 17:32:18 +0300

    addet vertical slider

---
+++ js/sliderVertical.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  $('.desc__header-slider').slick({
+    dots: true,
+    vertical: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    verticalSwiping: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    infinite: true,
+
+  });
+});
